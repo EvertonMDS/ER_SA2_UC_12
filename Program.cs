eaf3d51b971e704f5138c016c4b0fbefd19494ee
EvertonMDS <emds404@gmail.com>
2022-08-25 17:02:23 -0300

    ER-23/08/2022

--- Program.cs
+++ Program.cs
@@ -4,68 +4,112 @@
     {
         static void Main(string[] args)
         {
-            /*
-            PessoaFisica pf = new PessoaFisica();
-            pf.endereco = end;
-            pf.nome = "João da Silva Antunes";
-            pf.cpf = "65151546-20";
-            pf.dataNascimento = new DateTime(1982, 05, 19);
 
-            Console.WriteLine("**************************** Interpolação ***********************************");
 
-            //interpolação
-            Console.WriteLine($"O {pf.nome} mora na Rua: {pf.endereco.logradouro}, número {pf.endereco.numero}, cep:{end.CEP}");
-
-            Console.WriteLine("**********************''******* Concatenação ***********************'***********");
-
-            //concatenação
-            Console.WriteLine("O " + pf.nome + " mora na Rua: " + pf.endereco.logradouro + " número "  + pf.endereco.numero + " cep:" + end.CEP);
-
-            Console.WriteLine("***************** Forma despadronizada para executar a ação *********************");
-
-            //forma despadronizada para executar a ação
-            Console.WriteLine(pf.endereco.logradouro);
-            Console.WriteLine(pf.endereco.numero);
-            Console.WriteLine(pf.endereco.CEP);
-            Console.WriteLine(pf.endereco.complemento);
-
-            Console.WriteLine("***************************************************************");
-
-            bool idadeValida = pf.ValidarDataNascimento(pf.dataNascimento);
-
-            if (idadeValida == true)
+            static void BarraCarregamento(string texto)
             {
-                System.Console.WriteLine("Parabéns, você têm a idade apta para acessar o sistema.");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.Write(texto);
+                Thread.Sleep(500);
+
+                for (var contador = 0; contador < 10; contador++)
+                {
+                    Console.Write("$");
+                    Thread.Sleep(500);
+                }
             }
-            else
+
+            Console.WriteLine(@$"
+
+=================================================== 
+===================================================          
+!                                                 !
+!         Seja Bem Vindo ao nosso Sistema         !
+!             de Cadastro de Pessoa               !
+!               Fisica e Juridica:                !
+!                                                 !
+===================================================
+===================================================               
+");
+            //BarraCarregamento ("Start Sistema....")
+
+            string? opcao;
+
+            do
             {
-                Console.WriteLine("Você ainda não atingiu a idade.");
-            }
+                Console.WriteLine(@$"
+=================================================== 
+!     Escolha uma das opções abaixo + ENTER:      !          
+=================================================== 
+!              1 - Pessoa Física                  !
+!              2 - Pessoa Jurídica                !
+!                                                 !
+!              0 - Sair                           !
+!                                                 !
+===================================================               
+");
 
-            Console.WriteLine("***************************************************************");
-            }
-         */
+                opcao = Console.ReadLine();
 
-            PessoaJuridica pj = new PessoaJuridica ();
-            Endereco end = new Endereco();
-            end.logradouro = "Brigadeiro Augusto Carvalho Pereira";
-            end.numero = 380;
-            end.complemento = "Bloco-F";
-            end.CEP = "98037-000";
-            end.enderecoComercial = true;
+                switch (opcao)
+                {
+                    case "1":
+                        Endereco endPf = new Endereco();
 
-            pj.endereco = end;
-            pj.cnpj = "12345678990001";
-            pj.RazaoSocial = "Pessoa Juridica";
+                        endPf.logradouro = "Rua Brigadeiro Marco Antonio Costa";
+                        endPf.numero = 230;
+                        endPf.complemento = "Apt- Bloco - 2B";
+                        endPf.enderecoComercial = false;
 
-           if(pj.ValidarCNPJ(pj.cnpj))
-           {
-            Console.WriteLine("cnpj válido");
-           }
-            else
-            {
-               Console.WriteLine("cnpj invalido"); 
-            }
+                        PessoaFisica Creison = new PessoaFisica();
+                        Creison.cpf = "651683864546";
+                        Creison.dataNascimento = new DateTime(1984, 08, 21);
+                        Creison.endereco = endPf;
+                        Creison.nome = "Creison Tabajara";
+
+                        Console.WriteLine("Dados da Pessoa Física:");
+                        Console.WriteLine
+                        (@$"Nome: {Creison.nome}
+CPF: {Creison.cpf} 
+Nascido em {Creison.dataNascimento.ToString("dd/MM/yyyy")}
+Endereço: {endPf.logradouro}, {endPf.numero}");
+
+                        Console.WriteLine();
+                        break;
+                    case "2":
+                        Endereco endPj = new Endereco();
+
+                        endPj.logradouro = "Rua Niterói";
+                        endPj.numero = 180;
+                        endPj.complemento = "Escola SENAI";
+                        endPj.enderecoComercial = true;
+
+                        PessoaJuridica pj = new PessoaJuridica();
+                        pj.cnpj = "0104115487";
+                        pj.endereco = endPj;
+                        pj.RazaoSocial = "Pessoa Jurídica";
+                        pj.nome = "SENAI - Full Stack";
+
+                        Console.WriteLine("Dados da Pessoa Jurídica:");
+                        Console.WriteLine(@$"CNPJ: {pj.cnpj}, 
+Nome Fantasia: {pj.nome}, 
+Endereço da Empresa: {endPj.logradouro}, {endPj.numero}");
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Obrigado por utilizar o nosso sistema.");
+                        //  BarraCarregamento("Finalizando ");
+
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida, por favor digite uma das opções apresentadas.");
+                        break;
+                }
+            } while (opcao != "0");
+
+            Console.ResetColor();
         }
     }
 }
